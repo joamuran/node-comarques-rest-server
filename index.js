33d39246fc3add4f192e72d4461c1d5c404bb7bb
José A. Múrcia <joamuran@gmail.com>
2024-02-21 22:35:03 +0100

    Added countries

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import comarquesController from './APIExemples/comarques/controllers/comarquesController.js';
+import fs from 'fs';
 
 import bodyParser from 'body-parser';
 const { urlencoded, json } = bodyParser;
@@ -9,6 +10,11 @@ const app = express();
 
 // Habilitem CORS
 app.use(cors());
+
+// CArreguem regions i paisos
+const regions =  JSON.parse(fs.readFileSync('./data/regions.json'));
+const paisos =  JSON.parse(fs.readFileSync('./data/paisos.json'));
+
 
 app.use(urlencoded({ extended: true }));
 app.use(json());
@@ -32,5 +38,47 @@ comarquesRouter.get('/comarquesAmbImatge/:provincia', comarquesController.Obteni
 comarquesRouter.get('/infoComarca/:comarca', comarquesController.ObtenirInfoComarca);
 
 
+const paisosRouter = express.Router();
+
+/*Configurem el router de paisos a lo bruto */
+
+paisosRouter.get('/regions', (req, res) => {
+  res.json(regions.regions);
+});
+
+paisosRouter.get('/paisos/:regio', (req, res) => {
+  const regioNom = req.params.regio;
+  let llista = [];
+  for (let regio of paisos.paisos_europeus) {
+    if (regio.nom == regioNom) {
+      for (let pais of regio.paisos) {
+        llista.push(pais.nom);
+      }
+    }
+  }
+
+  res.send(llista);
+});
+
+paisosRouter.get('/paisos/info/:pais', (req, res) => {
+  try {
+    for (let regio of paisos.paisos_europeus) {
+      for (let pais of regio.paisos) {
+        if (pais.nom == req.params.pais)
+          res.send(pais);
+      }
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+
+
+
 app.use("/api/comarques", comarquesRouter);
+
+app.use("/api/europa", paisosRouter);
+
 app.use('*', DefaultController);
+
